7ca509206d5265bb1194a914f5452a27a616249e
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-02-28 10:38:35 +0100

    Always crate OC.Settings, even if not used
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- settings/src/main-admin-security.js
+++ settings/src/main-admin-security.js
@@ -6,5 +6,9 @@ __webpack_nonce__ = btoa(OC.requestToken)
 
 Vue.prototype.t = t;
 
+// Not used here but required for legacy templates
+window.OC = window.OC || {};
+window.OC.Settings = window.OC.Settings || {};
+
 const View = Vue.extend(AdminTwoFactor)
 new View().$mount('#two-factor-auth-settings')
